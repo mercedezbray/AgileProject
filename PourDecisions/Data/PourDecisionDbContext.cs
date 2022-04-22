c701574e9d70cb1aa540a771c1b39d021e64bb2d
Mercedez Bray <mercedezbray@gmail.com>
2022-04-22 17:23:48 -0400

    Migration and Scaffolding

---
+++ PourDecisions/Data/PourDecisionDbContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using PourDecisions.Models;
+
+namespace PourDecisions.Data
+{
+    public partial class PourDecisionDbContext : DbContext
+    {
+        public PourDecisionDbContext()
+        {
+        }
+
+        public PourDecisionDbContext(DbContextOptions<PourDecisionDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<Order> Orders { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=ConnectionString:PourDecisionDb");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.AlcType)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("FK__Orders__ProductI__4222D4EF");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 0)");
+
+                entity.HasOne(d => d.CategoryNavigation)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.Category)
+                    .HasConstraintName("FK__Products__Catego__3F466844");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
