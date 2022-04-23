6570e252f38e9a73c5b28a563d636eaee5ca842b
brandon fourthman <bhf0714@gmail.com>
2022-04-23 14:05:14 -0400

    first commit

--- sql/PopulatingDB.sql
+++ sql/PopulatingDB.sql
@@ -2,5 +2,4 @@ INSERT into Categories (AlcType)
 VALUES ('Vodka'), ('Tequila'), ('Gin'), ('Whiskey'), ('Rum'), ('Brandy'), ('Wine'), ('Beer')
 
 INSERT into Products (Category, Name, Price)
-Values (1,'Titos', 19.99), (1, 'Svedka', 9.99), (2, 'Hornitos', 19.99), (2, 'Patron', 49.99), (3, 'Aviation', 19.99), (3, 'Tanqueray', 44.99), (4, 'Makers Mark', 25.99), (4, 'Jim Beam', 15.99), (5, 'Captain Morgan', 19.99), (5, 'Bacardi', 11.99),
-(6, 'Hennessey', 49.99), (6, 'E&J', 11.99), (7, 'Soft Red', 7.19), (7, 'Camelot Honey Mead', 11.99), (8, 'Natty Light', 5.99), (8, 'Voodoo Ranger', 18.89)
+Values (1,'Titos', 19.99), (1, 'Svedka', 9.99), (2, 'Hornitos', 19.99), (2, 'Patron', 49.99), (3, 'Aviation', 19.99), (3, 'Tanqueray', 44.99), (4, 'Makers Mark', 25.99), (4, 'Jim Beam', 15.99), (5, 'Captain Morgan', 19.99), (5, 'Bacardi', 11.99), (6, 'Hennessey', 49.99), (6, 'E&J', 11.99), (7, 'Soft Red', 7.19), (7, 'Camelot Honey Mead', 11.99), (8, 'Natty Light', 5.99), (8, 'Voodoo Ranger', 18.89)
